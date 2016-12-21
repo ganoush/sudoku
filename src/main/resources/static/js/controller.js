1eebb59d181fe25ae60cf4c425297813a7299939
ganeshnagarajan <ganeshatmail@gmail.com>
2016-12-20 22:00:04 -0500

    Load Sudokuboard UI commit

--- src/main/resources/static/js/controller.js
+++ src/main/resources/static/js/controller.js
@@ -2,10 +2,16 @@
  * Created by ganeshnagarajan on 12/19/16.
  */
 Sudoku.controller('SudokuController', function SudokuController($scope, SudokuService) {
-    $scope.sudokuBoard = {};
-    $scope.messages = {};
-    SudokuService.loadBoard().success(function(response){
-        $scope.sudokuBoard = response.sudokuBoard;
-        $scope.messages = 'Sudoku Board loaded successfully!';
-    });
+    $scope.sudokuBoard = '';
+    $scope.messages = '';
+    $scope.loadBoard = function(){
+        SudokuService.loadBoard().success(function(response){
+            $scope.sudokuBoard = response.sudokuBoard;
+            //$scope.messages = 'Sudoku Board loaded successfully!';
+        });
+    };
+    $scope.validateMove = function(row, cols, value){
+        //console.log(value);
+        //Invoke the put service to validate move and shovel the error messages to messages scope
+    };
 });
