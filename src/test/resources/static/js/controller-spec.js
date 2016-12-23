0a4e517be021d87e35b15ea1fce6a3702e8a5222
ganeshnagarajan <ganeshatmail@gmail.com>
2016-12-22 20:46:48 -0500

    UI mock test cases checkin

---
+++ src/test/resources/static/js/controller-spec.js
@@ -0,0 +1,50 @@
+describe('controller', function () {
+    var $scope, SudokuService;
+
+    beforeEach(module('Sudoku'));
+
+    beforeEach(inject(function($injector){
+        $scope = $injector.get('$rootScope').$new();
+    }));
+
+    var sudokuLoadResponse = {
+        success: function (func) {
+            func({ sudokuBoard: [[1],[2]], validMove: true, sudokuComplete: true});
+        }
+    };
+
+    var sudokuValidateMoveResponse = {
+        success: function (func) {
+            func({ validMove: false, sudokuComplete: false, validationMessages: ['Please enter a valid number']});
+        }
+    };
+
+    var sudokuServiceMock = {
+        loadBoard: function(){
+            return sudokuLoadResponse;
+        },
+        validateMove: function(){
+            return sudokuValidateMoveResponse;
+        }
+    };
+
+    it('should load board', function(){
+         inject(function($controller){
+             $controller('SudokuController', { $scope: $scope, SudokuService: sudokuServiceMock } );
+             $scope.loadBoard();
+             expect($scope.sudokuBoard.length).toEqual(2);
+             $scope.validateMove(1,2,3);
+             expect($scope.messages.length).toEqual(1);
+             expect($scope.messages[0]).toEqual('Please enter a valid number');
+         });
+    });
+
+    it('should validate move ', function(){
+        inject(function($controller){
+            $controller('SudokuController', { $scope: $scope, SudokuService: sudokuServiceMock } );
+            $scope.validateMove(1,2,3);
+            expect($scope.messages.length).toEqual(1);
+            expect($scope.messages[0]).toEqual('Please enter a valid number');
+        });
+    });
+});
